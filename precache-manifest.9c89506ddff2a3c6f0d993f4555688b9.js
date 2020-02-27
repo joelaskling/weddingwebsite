88749277b5ff30bd9ba0a5a466c92582c86ed3ca
System Administrator <root@Joels-MBP.lan>
2020-02-27 15:29:22 +0100

    Updates

---
+++ precache-manifest.9c89506ddff2a3c6f0d993f4555688b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5279716e44770c8ca80fe929090b8cc",
+    "url": "/weddingwebsite/index.html"
+  },
+  {
+    "revision": "6b84ba9ca0d98ebb3af8",
+    "url": "/weddingwebsite/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8b997d456ca2f39459e0",
+    "url": "/weddingwebsite/static/js/2.184e4515.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/weddingwebsite/static/js/2.184e4515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b84ba9ca0d98ebb3af8",
+    "url": "/weddingwebsite/static/js/main.a1281801.chunk.js"
+  },
+  {
+    "revision": "f5fbc1f390ee24098a7c",
+    "url": "/weddingwebsite/static/js/runtime-main.b44a3c47.js"
+  },
+  {
+    "revision": "e75a4315abf483c3ea031d56f9df9ea4",
+    "url": "/weddingwebsite/static/media/hulta.e75a4315.jpg"
+  }
+]);
